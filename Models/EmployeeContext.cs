cd4c67b80ef3dfdaa8c64f71645701cf13c263e8
katpap17 <58397102+katpap17@users.noreply.github.com>
2021-03-14 13:20:23 +0200

    Add files via upload

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementApp.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+   : base(options)
+        {
+        }
+
+        public DbSet<Employee> employee { get; set; }
+    }
+}
---
+++ Models/SkillContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementApp.Models
+{
+    public class SkillContext : DbContext
+    {
+        public SkillContext(DbContextOptions<SkillContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Skill> Skill { get; set; }
+    }
+}
